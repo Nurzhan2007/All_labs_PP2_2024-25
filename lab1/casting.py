8d57fd4e0b1f94a49af02009b82456f4db0b9338
Нуржан Казыгул <nurzankazygul@MacBook-Air-Nurzan.local>
2025-01-31 11:10:02 +0500

    Первый коммит

---
+++ lab1/casting.py
@@ -0,0 +1,5 @@
+x="you instail a "
+y=int(99.9)
+z=float(1.1)
+print(x,y,"percent")
+print("left",z)
---
+++ lab1/variables.py
@@ -0,0 +1,9 @@
+w = 15
+ww= "week"
+githubsubmission = "11 ball for submission"
+defense = "49 ball for defense"
+s="syllabus have "
+p="PP2 Spring's "
+f=40
+
+print (p,s,w,ww,"and",githubsubmission,defense)
